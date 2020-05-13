f6a9aec5a50bbb885fcb20ac755101a11b69b5fa
Jan Christoph Bernack <jc.bernack@gmail.com>
2020-05-13 22:14:23 +0200

    improve update script

--- update.sh
+++ update.sh
@@ -1,11 +1,3 @@
-#! /bin/sh
+#!/bin/sh
 rm -rf $2
 git clone $1 $2
-cd $2
-rm -rf .git .github Documentation Projects Middlewares/Third_Party Utilities/Media Utilities/PC_Software CONTRIBUTING.md CODE_OF_CONDUCT.md README.md
-find . -name _htmresc -exec rm -rf {} \;
-find . -name docs -exec rm -rf {} \;
-find . -name Documentation -exec rm -rf {} \;
-find . -name "*.pdf" -exec rm -rf {} \;
-find . -name "*.rtf" -exec rm -rf {} \;
-cd ..
---
+++ update_all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+./update.sh git@github.com:STMicroelectronics/STM32CubeF0.git f0
+./update.sh git@github.com:STMicroelectronics/STM32CubeF1.git f1
+./update.sh git@github.com:STMicroelectronics/STM32CubeF2.git f2
+./update.sh git@github.com:STMicroelectronics/STM32CubeF3.git f3
+./update.sh git@github.com:STMicroelectronics/STM32CubeF4.git f4
+./update.sh git@github.com:STMicroelectronics/STM32CubeF7.git f7
+./update.sh git@github.com:STMicroelectronics/STM32CubeH7.git h7
+./update.sh git@github.com:STMicroelectronics/STM32CubeL0.git l0
+./update.sh git@github.com:STMicroelectronics/STM32CubeL1.git l1
+./update.sh git@github.com:STMicroelectronics/STM32CubeL4.git l4
