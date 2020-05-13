d50ca26836573e4963bfb341c87071a17319f897
Jan Christoph Bernack <jc.bernack@gmail.com>
2020-05-13 20:15:50 +0200

    improve update script

--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@
 rm -rf $2
 git clone $1 $2
 cd $2
-rm -rf .git .github Documentation Projects Drivers/CMSIS/docs Middlewares/Third_Party Utilities/Media CONTRIBUTING.md CODE_OF_CONDUCT.md README.md
+rm -rf .git .github Documentation Projects Middlewares/Third_Party Utilities/Media Utilities/PC_Software CONTRIBUTING.md CODE_OF_CONDUCT.md README.md
 find . -name _htmresc -exec rm -rf {} \;
+find . -name docs -exec rm -rf {} \;
+find . -name Documentation -exec rm -rf {} \;
+find . -name "*.pdf" -exec rm -rf {} \;
+find . -name "*.rtf" -exec rm -rf {} \;
 cd ..
