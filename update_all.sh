920c391d9ff10cf60ff24df14637de50e9fd19c0
Jan Christoph Bernack <jc.bernack@gmail.com>
2020-05-19 18:47:09 +0200

    add support for G0 MCUs

--- update_all.sh
+++ update_all.sh
@@ -5,6 +5,7 @@
 ./update.sh git@github.com:STMicroelectronics/STM32CubeF3.git f3
 ./update.sh git@github.com:STMicroelectronics/STM32CubeF4.git f4
 ./update.sh git@github.com:STMicroelectronics/STM32CubeF7.git f7
+./update.sh git@github.com:STMicroelectronics/STM32CubeG0.git g0
 ./update.sh git@github.com:STMicroelectronics/STM32CubeH7.git h7
 ./update.sh git@github.com:STMicroelectronics/STM32CubeL0.git l0
 ./update.sh git@github.com:STMicroelectronics/STM32CubeL1.git l1
